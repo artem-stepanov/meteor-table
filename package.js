581286dc2b2639c22e785cd77b3d64e9162633c1
Artem Stepanov <me@artemstepanov.com>
2015-09-10 16:45:01 +0300

    ver 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akora:meteor-table',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'A rich reactive table for Meteor',
   git: 'https://github.com/artem-stepanov/meteor-table.git',
   documentation: 'README.md'
@@ -11,6 +11,7 @@ Package.onUse(function(api) {
   api.use('templating', 'client');
   api.use('jquery', 'client');
   api.use('underscore', 'client');
+  api.use('spacebars', 'client');
   api.use('reactive-var@1.0.3', 'client');
   //api.use("anti:i18n@0.4.3", 'client');
   api.use("mongo@1.0.8", ["server", "client"]);
@@ -27,12 +28,17 @@ Package.onUse(function(api) {
   api.use("momentjs:moment@2.10.6", ["server", "client"]);
   api.use("suxez:jquery-serialize-object@1.0.0", "client");
 
+  api.addFiles(['lib/MeteorTable.html', 'lib/MeteorTable.css'],  'client');
   api.addFiles('lib/MeteorTable.js',  ['client', 'server']);
-  api.addFiles('lib/MeteorTable.css',  'client');
-  api.addFiles('lib/MeteorTable.html',  'client');
+  api.addFiles('lib/MeteorTableClient.js', 'client');
   api.addFiles('lib/MeteorTableServer.js', 'server');
 
   api.export('MeteorTable', ['client', 'server']);
+  api.export('MeteorTableField', ['client', 'server']);
+  api.export('METEOR_TABLE_FIELD_TYPE_STRING', ['client', 'server']);
+  api.export('METEOR_TABLE_FIELD_TYPE_TEXT', ['client', 'server']);
+  api.export('METEOR_TABLE_FIELD_TYPE_DATE', ['client', 'server']);
+  api.export('METEOR_TABLE_FIELD_TYPE_SELECT', ['client', 'server']);
 });
 
 //Package.onTest(function(api) {
