c9dbf7f73faeb400f46768604d5f0f8b7e08d64e
Artem Stepanov <me@artemstepanov.com>
2016-07-02 18:29:52 +0300

    fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akora:meteor-table',
-  version: '0.2.8',
+  version: '0.2.9',
   summary: 'A rich reactive table for Meteor',
   git: 'https://github.com/artem-stepanov/meteor-table.git',
   documentation: 'README.md'
