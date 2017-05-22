69cc74dad86c0c8ce625988dd3a85b31d3ef3a3a
Artem Stepanov <me@artemstepanov.com>
2017-05-22 17:49:43 +0300

    Css improve, ver 0.2.27

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akora:meteor-table',
-  version: '0.2.27',
+  version: '0.2.28',
   summary: 'A rich reactive table for Meteor',
   git: 'https://github.com/artem-stepanov/meteor-table.git',
   documentation: 'README.md'
